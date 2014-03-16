cff48c118d8c209d174e8d1eb84149d16845e0f2
Cmput 274/275 <student@cmput274-vm.(none)>
2014-03-16 17:23:16 -0600

    Simple hello world

---
+++ screen.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((640,480),0,24)
+pygame.display.set_caption("MONSTUHS")
+create = pygame.font.SysFont("comicsansms",30)
+f = create.render("MONSTUHS", True, (0,0,0), (255,255,255))
+img=pygame.image.load("simple.jpg").convert()
+
+while True:
+    for i in pygame.event.get():
+        if i.type == QUIT:
+            exit()
+
+screen.blit(img,(0,0))
+screen.blit(f,(200,200))
+pygame.display.update()
