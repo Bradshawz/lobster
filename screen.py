316d2f4b0af277eb58c574f94ecb0b13eb8717a0
Ross Anderson <ross.anderson87@gmail.com>
2014-03-25 10:27:47 -0600

    Variable name semantics in screen.py

--- screen.py
+++ screen.py
@@ -5,15 +5,17 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((640,480),0,24)
 pygame.display.set_caption("MONSTUHS")
-create = pygame.font.SysFont("comicsansms",30)
-f = create.render("MONSTUHS", True, (0,0,0), (255,255,255))
-img=pygame.image.load("simple.jpg").convert()
+game_font = pygame.font.SysFont("comicsansms",30)
+text = game_font.render("MONSTUHS", True, (0,0,0), (255,255,255))
+# img=pygame.image.load("simple.jpg").convert()
+
+# screen.blit(img,(0,0))
+screen.blit(text,(200,200))
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             exit()
 
-screen.blit(img,(0,0))
-screen.blit(f,(200,200))
-pygame.display.update()
+
