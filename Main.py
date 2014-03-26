1f88556b7e8cc8195aea1cea0ee2583b5b25632a
Ross Anderson <ross.anderson87@gmail.com>
2014-03-25 23:04:52 -0600

    Player can be moved with arrow keys. Next: either make a floor and walls and do collision, then do gravity

--- Main.py
+++ Main.py
@@ -2,20 +2,68 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+from Player import *
+
+# Init pygame & create a screen
 pygame.init()
 screen = pygame.display.set_mode((640,480),0,24)
+
+# Create a white background
+bg = pygame.Surface(screen.get_size())
+bg = bg.convert()
+bg.fill(pygame.Color(255,255,255))
+
+# Set the window title and game font
 pygame.display.set_caption("Horde")
 game_font = pygame.font.SysFont("comicsansms",30)
-text = game_font.render("Horde", True, (0,0,0), (255,255,255))
-image = pygame.image.load("player-tmp.png").convert_alpha()
 
-screen.blit(image,(0,0))
-screen.blit(text,(200,200))
+# Create the player
+playerGroup = pygame.sprite.GroupSingle() # Create the Group
+player = Player("player-tmp.png") # Create the player Sprite
+player.add(playerGroup) # Add the player Sprite to the Group
+
 pygame.display.update()
 
+# --------------------------------------------
+# Main Game Loop
+# --------------------------------------------
 while True:
+    # --------------------------------------------
+    # Key Press Handling
+    # --------------------------------------------
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             exit()
-
-
+    pygame.event.pump()
+    
+    # --------------------------------------------
+    # Key Press Handling
+    # --------------------------------------------
+    
+    # If any keys are down
+    if pygame.key.get_focused():
+        # Get a list of all keys pressed right now
+        key_down = pygame.key.get_pressed()
+        
+        if key_down[pygame.K_LEFT]:
+            player.move(-1, 0)
+        if key_down[pygame.K_RIGHT]:
+            player.move(1, 0)
+        if key_down[pygame.K_UP]:
+            player.move(0, -1)
+        if key_down[pygame.K_DOWN]:
+            player.move(0, 1)
+    
+    # --------------------------------------------
+    # Redrawing
+    # --------------------------------------------
+    
+    # Redraw the Background
+    screen.blit(bg, (0,0))
+    
+    # Redraw all Groups
+    playerGroup.draw(screen)
+    # enemiesGroup.draw(screen) # to be added in later
+    
+    # Update the display
+    pygame.display.update()
--- Player.py
+++ Player.py
@@ -1,20 +1,21 @@
 import pygame
+from pygame import Rect
 
 class Player(pygame.sprite.Sprite):
-    '''
+    """
     Model for the Player character.
     
     Can move, jump, attack.
     Has an image, health, rect-bounds.
-    '''
+    """
     BASE_HEALTH = 10
     BASE_MOVE_SPEED = 1
 
-    def __init__(self, color, width, height):
-        '''
-        For now, pass in a color, width and height, and
-        a block will be created from the given params.
-        '''
+    def __init__(self, image_filename):
+        """
+        Pass in the filename of the image to represent
+        this player.
+        """
         # ------------------------------------------------
         # Call super constructor, set image and rect
         # ------------------------------------------------
@@ -22,10 +23,8 @@ class Player(pygame.sprite.Sprite):
         # Call the parent class (Sprite) constructor)
         pygame.sprite.Sprite.__init__(self)
         
-        # Create an image of the block, and fill it with a color.
-        # In the future this will be an image.
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        # Set the image of the player Sprite
+        self.image = pygame.image.load(image_filename).convert_alpha()
         
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
@@ -35,4 +34,26 @@ class Player(pygame.sprite.Sprite):
         # ------------------------------------------------
         self.health = Player.BASE_HEALTH
         self.move_speed = Player.BASE_MOVE_SPEED
+    
+    def move(self, x, y):
+        self.move_game_coords(x, y)
         
+    def move_game_coords(self, x, y):
+        """
+        Moves the player the specified number of pixels in the
+        x and y direction.
+        
+        y+ is DOWN, x+ is right
+        """
+        self.rect.x += x
+        self.rect.y += y
+        
+    def move_std_coords(self, x, y):
+        """
+        Moves the player the specified number of pixels in the
+        x and y direction.
+        
+        y+ is UP, x+ is right
+        """
+        self.rect.x += x
+        self.rect.y -= y
