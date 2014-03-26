f9578136d4dd2eaba1d23f0a32c1213c4c527d19
Ross Anderson <ross.anderson87@gmail.com>
2014-03-25 22:07:30 -0600

    Png image draws with transparency

--- Main.py
+++ Main.py
@@ -4,12 +4,12 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((640,480),0,24)
-pygame.display.set_caption("MONSTUHS")
+pygame.display.set_caption("Horde")
 game_font = pygame.font.SysFont("comicsansms",30)
-text = game_font.render("MONSTUHS", True, (0,0,0), (255,255,255))
-# img=pygame.image.load("simple.jpg").convert()
+text = game_font.render("Horde", True, (0,0,0), (255,255,255))
+image = pygame.image.load("player-tmp.png").convert_alpha()
 
-# screen.blit(img,(0,0))
+screen.blit(image,(0,0))
 screen.blit(text,(200,200))
 pygame.display.update()
 
