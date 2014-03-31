57f02ce0410b8bae1a21af7ea032b1be5b0ef222
Ross Anderson <ross.anderson87@gmail.com>
2014-03-31 08:59:48 -0600

    Start of collision detection

--- Main.py
+++ Main.py
@@ -2,11 +2,19 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
-from Player import *
+import Character, Player, Block
+
+# Window Constants, Application Constants
+# TODO refactor these into a Screen/Window/etc class
+WINDOW_HEIGHT = 480
+WINDOW_WIDTH = 640
 
 # Init pygame & create a screen
 pygame.init()
-screen = pygame.display.set_mode((640,480),0,24)
+screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT),0,24)
+
+# Create a clock to use to hold the framerate constant
+clock = pygame.time.Clock()
 
 # Create a white background
 bg = pygame.Surface(screen.get_size())
@@ -22,14 +30,17 @@ playerGroup = pygame.sprite.GroupSingle() # Create the Group
 player = Player("player_tmp.png") # Create the player Sprite
 player.add(playerGroup) # Add the player Sprite to the Group
 
-pygame.display.update()
+# Create the map
+# TODO refactor this into a Map or Level class maybe, i.e. Level.load, for block in Level.blocks, etc
+blockGroup = pygame.sprite.Group()
+platform_test = Block("platform_tile_blue.png", x=WINDOW_WIDTH/2, y=WINDOW_HEIGHT/2).add(blockGroup)
 
 # --------------------------------------------
 # Main Game Loop
 # --------------------------------------------
 while True:
     # --------------------------------------------
-    # Key Press Handling
+    # Event Handling
     # --------------------------------------------
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,22 +48,35 @@ while True:
     pygame.event.pump()
     
     # --------------------------------------------
-    # Key Press Handling
+    # Movement & Collisions
     # --------------------------------------------
     
-    # If any keys are down
+    # Player keyboard movement
     if pygame.key.get_focused():
-        # Get a list of all keys pressed right now
-        key_down = pygame.key.get_pressed()
+        key_down = pygame.key.get_pressed() # Get a list of all keys pressed right now
         
         if key_down[pygame.K_LEFT]:
-            player.move(-1, 0)
-        if key_down[pygame.K_RIGHT]:
-            player.move(1, 0)
+            player.accel_left()
+        elif key_down[pygame.K_RIGHT]:
+            player.accel_right()
+        else:
+            player.decel
         if key_down[pygame.K_UP]:
-            player.move(0, -1)
+            player.accel_up()
         if key_down[pygame.K_DOWN]:
-            player.move(0, 1)
+            player.accel_down()
+            
+    # Player Collision with Blocks
+    collisions = pygame.sprite.groupcollide(playerGroup, blockGroup, False, False)
+    for player_sprite, block_sprites in collisions.items():
+        for block_sprite in block_sprites:
+            # If their new rects collide, use their pixel mask to check if they really do collide
+            if pygame.sprite.collide_mask(player_sprite, block_sprite):
+                # COLLISION!
+                player_sprite.handle_static_collision(block_sprite)
+    
+    # Actually move the player to where it should be
+    player.move()
     
     # --------------------------------------------
     # Redrawing
@@ -63,7 +87,13 @@ while True:
     
     # Redraw all Groups
     playerGroup.draw(screen)
+    blockGroup.draw(screen)
     # enemiesGroup.draw(screen) # to be added in later
     
     # Update the display
     pygame.display.update()
+    
+    # --------------------------------------------
+    # Clock Tick
+    # --------------------------------------------
+    clock.tick(120)
--- Player.py
+++ Player.py
@@ -1,71 +1,21 @@
-import pygame
+import Character
 
-class Player(pygame.sprite.Sprite):
+class Player(Character):
     """
-    Player character.
+    Player character. Subclasses Character->Sprite.
     
     Can move, jump, attack.
     Has an image, health, rect-bounds.
     """
     BASE_HEALTH = 10
-    BASE_MOVE_SPEED = 1
-
+    
     def __init__(self, image_filename):
-        """
-        Pass in the filename of the image to represent
-        this player.
-        """
-        # ------------------------------------------------
-        # Call super constructor, set image and rect
-        # ------------------------------------------------
+        super(Character, self).__init__(image_filename)
         
-        # Call the parent class (Sprite) constructor)
-        pygame.sprite.Sprite.__init__(self)
+        self.on_ground = False
+        self.health = self.BASE_HEALTH
         
-        # Set the image of the player Sprite
-        self.image = pygame.image.load(image_filename).convert_alpha()
-        
-        # Fetch the rectangle object that has the dimensions of the image
-        self.rect = self.image.get_rect()
-        
-        # ------------------------------------------------
-        # Initialize player-specific variables
-        # ------------------------------------------------
-        self.health = Player.BASE_HEALTH
-        self.move_speed = Player.BASE_MOVE_SPEED
-        
-    """
-    Movement
-    """
+    def jump(self):
+        pass
+
     
-    def move_left(self):
-        self._move_game_coords(-1 * Player.BASE_MOVE_SPEED, 0)
-        
-    def move_right(self):
-        self._move_game_coords(Player.BASE_MOVE_SPEED, 0)
-    
-    def move_up(self):
-        self._move_game_coords(0, -1 * Player.BASE_MOVE_SPEED)
-        
-    def move_down(self):
-        self._move_game_coords(0, Player.BASE_MOVE_SPEED)
-        
-    def _move_game_coords(self, x, y):
-        """
-        Moves the player the specified number of pixels in the
-        x and y direction.
-        
-        y+ is DOWN, x+ is right
-        """
-        self.rect.x += x
-        self.rect.y += y
-        
-    def _move_math_coords(self, x, y):
-        """
-        Moves the player the specified number of pixels in the
-        x and y direction.
-        
-        y+ is UP, x+ is right
-        """
-        self.rect.x += x
-        self.rect.y -= y
